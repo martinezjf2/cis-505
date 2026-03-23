04e3389ce728d09c14cd8c7a0efef8989e162464
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2026-03-22 23:05:19 -0400

    Completed Module 1 HelloWorldApp

---
+++ Module_1/HelloWorldApp/MartinezHelloWorld.java
@@ -0,0 +1,11 @@
+package Module_1.HelloWorldApp;
+
+public class MartinezHelloWorld {
+
+    public static void main(String[] args) {
+        String firstName = "Jeffrey";
+        String lastName = "Martinez";
+
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
